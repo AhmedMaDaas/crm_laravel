63dfd9e0ac25d364810f4691a8742225dc79ab2a
Ahmed Daas <ahmedhamadah973@gmail.com>
2022-07-18 21:43:44 +0400

    v2

--- routes/Users/users.php
+++ routes/Users/users.php
@@ -1,15 +1,18 @@
 <?php
 
-Route::group(['prefix' => '/users', 'namespace' => 'User', 'middleware' => ['auth','admin']],function(){
+Route::group(['prefix' => '/users', 'namespace' => 'User', 'middleware' => ['auth']],function(){
+    
+    Route::get('/profile','UsersController@userProfile')->name('users.profile');
 
 	// user route
-    Route::get('/','UsersController@index')->name('users.index');
-    Route::get('/profile','UsersController@userProfile')->name('users.profile');
-    Route::get('/show','UsersController@show')->name('users.show');
-    Route::get('/edit','UsersController@edit')->name('users.edit');
-    Route::get('/create','UsersController@create')->name('users.create');
-    Route::post('/store','UsersController@store')->name('users.store');
-    Route::put('/update','UsersController@update')->name('users.update');
-    Route::delete('/destroy','UsersController@destroy')->name('users.destroy');
+	Route::group(['middleware' => ['admin']], function(){
+		Route::get('/','UsersController@index')->name('users.index');
+	    Route::get('/show','UsersController@show')->name('users.show');
+	    Route::get('/edit','UsersController@edit')->name('users.edit');
+	    Route::get('/create','UsersController@create')->name('users.create');
+	    Route::post('/store','UsersController@store')->name('users.store');
+	    Route::put('/update','UsersController@update')->name('users.update');
+	    Route::delete('/destroy','UsersController@destroy')->name('users.destroy');
+	});
 
 });
--- routes/Users/users_api.php
+++ routes/Users/users_api.php
@@ -1,13 +1,16 @@
 <?php
 
-Route::group(['prefix' => '/users', 'namespace' => 'User', 'middleware' => ['auth:sanctum', 'admin']],function(){
+Route::group(['prefix' => '/users', 'namespace' => 'User', 'middleware' => ['auth:sanctum']],function(){
 
 	// user route
-    Route::get('/','UsersAPIController@index');
     Route::get('/profile','UsersAPIController@userProfile');
-    Route::get('/show','UsersAPIController@show');
-    Route::post('/create','UsersAPIController@create');
-    Route::post('/update','UsersAPIController@update');
-    Route::delete('/destroy','UsersAPIController@destroy');
+
+    Route::group(['middleware' => ['admin']], function(){
+		Route::get('/','UsersAPIController@index');
+	    Route::get('/show','UsersAPIController@show');
+	    Route::post('/create','UsersAPIController@create');
+	    Route::post('/update','UsersAPIController@update');
+	    Route::delete('/destroy','UsersAPIController@destroy');
+	});
 
 });
