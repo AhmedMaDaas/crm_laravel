b5caf98157c6b8432024f64efbfb8cbe9f685793
Ahmed Daas <ahmedhamadah973@gmail.com>
2022-07-18 18:20:53 +0400

    v2

--- app/Http/Middleware/Admin.php
+++ app/Http/Middleware/Admin.php
@@ -4,6 +4,7 @@ namespace App\Http\Middleware;
 
 use Closure;
 use Illuminate\Support\Facades\Auth;
+use App\Exceptions\RequestException;
 
 class Admin
 {
@@ -14,10 +15,15 @@ class Admin
      * @param  \Closure  $next
      * @return mixed
      */
+    private $message = 'Unauthorized';
+
     public function handle($request, Closure $next)
     {
         if(Auth::check() && Auth::user()->isAdmin()){
             return $next($request);
+        }
+        else if($request->is('api/*')){
+            throw new RequestException($this->message, null, 403);
         }
         else{
             request()->session()->flash('error','You do not have any permission to access this page');
